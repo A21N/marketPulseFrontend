d033a3439fa34562622c82fcbdb1bdafef37ef2a
A21N <alindorobantu95@gmail.com>
2024-07-28 21:13:19 +0300

    update Priceomv

--- src/components/content/principala/omv/Priceomv.jsx
+++ src/components/content/principala/omv/Priceomv.jsx
@@ -4,45 +4,29 @@ export const Priceomv = () => {
   const containerRef = useRef(null);
 
   useEffect(() => {
-    const scriptId = 'tradingview-widget-script-price-omv';
-
-    // Verifică și elimină orice script existent
-    const existingScript = document.getElementById(scriptId);
-    if (existingScript) {
-      existingScript.remove();
-    }
-
-    // Adaugă scriptul TradingView
-    const script = document.createElement('script');
-    script.id = scriptId;
-    script.type = 'text/javascript';
-    script.src = 'https://s3.tradingview.com/external-embedding/embed-widget-single-quote.js';
-    script.async = true;
-    script.innerHTML = JSON.stringify({
-      symbol: "BVB:SNP",
-      width: 350,
-      isTransparent: false,
-      colorTheme: "light",
-      locale: "en"
-    });
-
-    if (containerRef.current) {
+    // Verifică dacă scriptul a fost deja adăugat
+    if (!document.getElementById('tradingview-widget-script')) {
+      const script = document.createElement('script');
+      script.id = 'tradingview-widget-script';
+      script.type = 'text/javascript';
+      script.src = 'https://s3.tradingview.com/external-embedding/embed-widget-single-quote.js';
+      script.async = true;
+      script.innerHTML = JSON.stringify({
+        symbol: "BVB:SNP",
+        width: 350,
+        isTransparent: false,
+        colorTheme: "light",
+        locale: "en"
+      });
       containerRef.current.appendChild(script);
     }
-
-    return () => {
-      if (script) {
-        script.remove();
-      }
-    };
   }, []);
 
   return (
     <div className="tradingview-widget-container" ref={containerRef}>
       <div className="tradingview-widget-container__widget"></div>
-      <div className="tradingview-widget-copyright">
-       
-      </div>
+      <div className="tradingview-widget-copyright"></div>
     </div>
   );
 };
+
