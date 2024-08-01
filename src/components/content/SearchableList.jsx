c5f20320150ead62f0478bc09e57cb92d24ae278
A21N <alindorobantu95@gmail.com>
2024-08-01 17:29:39 +0300

    am finalizat casuta de search de pe prima pagina

--- src/components/content/SearchableList.jsx
+++ src/components/content/SearchableList.jsx
@@ -1,36 +1,91 @@
 import React, { useState } from 'react';
 import '../../css/App.css';
+
 const SearchableList = () => {
     const [searchTerm, setSearchTerm] = useState('');
+    
     const companies = [
-        'OMV Petrom',
-        'Antibiotice',
-        'Bermas',
-        'Biofarm',
-        'BRD',
-        'Aerostar',
-        'Alro',
-        'Banca Transilvania',
-        'Compet',
-        'Compa',
-        'Electrica',
-        'Electromagnetica',
-        'Hidroelectrica',
-        'Medlife',
-        'Nuclearelectrica',
-        'OneUnitedProperty',
-        'Purcari',
-        'Romgaz',
-        'Teraplast',
-        'Transelectrica'
+        'Sphera Franchise Group - SFG',
+        'AAGES S.A. - AAG',
+        'ALUMIL ROM INDUSTRY S.A. - ALU',
+        'AQUILA PART PROD COM - AQ',
+        'AEROSTAR S.A. - ARS',
+        'ARTEGO SA - ARTE',
+        'ANTIBIOTICE S.A. - ATB',
+        'BIOFARM S.A. - BIO',
+        'BRD - GROUPE SOCIETE GENERALE S.A. - BRD',
+        'SSIF BRK FINANCIAL GROUP SA - BRK',
+        'BERMAS S.A. - BRM',
+        'F.I.A.I.R. BET FI Index Invest - BTF',
+        'BURSA DE VALORI BUCURESTI SA - BVB',
+        'SIF HOTELURI SA - CAOR',
+        'CARBOCHIM S.A. - CBC',
+        'COMELF S.A. - CMF',
+        'COMPA S.A. - CMP',
+        'CONPET SA - COTE',
+        'CHIMCOMPLEX S.A. BORZESTI - CRC',
+        'Digi Communications N.V. - DIGI',
+        'TURISM, HOTELURI, RESTAURANTE MAREA NEAGRA S.A. - EFO',
+        'ELECTROMAGNETICA SA - ELMA',
+        'SOCIETATEA ENERGETICA ELECTRICA S.A. - EL',
+        'EVERGENT INVESTMENTS S.A. - EVER',
+        'FONDUL PROPRIETATEA - FP',
+        'S.P.E.E.H. HIDROELECTRICA S.A. - H2O',
+        'IAR SA Brașov - IARV',
+        'INFINITY CAPITAL INVESTMENTS S.A. - INFINITY',
+        'LION CAPITAL S.A. - LION',
+        'OIL TERMINAL S.A. - OIL',
+        'ONE UNITED PROPERTIES - ONE',
+        'PREFAB SA - PREH',
+        'ROMPETROL WELL SERVICES S.A. - PTR',
+        'FARMACEUTICA REMEDIA SA - RMAH',
+        'ROMCARBON SA - ROCE',
+        'SAFETECH INNOVATIONS - SAFE',
+        'LION CAPITAL S.A. - SIF1',
+        'INFINITY CAPITAL INVESTMENTS S.A. - SIF5',
+        'S.N.G.N. ROMGAZ S.A. - SNG',
+        'S.N. NUCLEARELECTRICA S.A. - SNN',
+        'SANTIERUL NAVAL ORSOVA S.A. - SNO',
+        'OMV PETROM S.A. - SNP',
+        'SOCEP S.A. - SOCP',
+        'TRANSILVANIA BROKER DE ASIGURARE - TBK',
+        'TURBOMECANICA S.A. - TBM',
+        'C.N.T.E.E. TRANSELECTRICA - TEL',
+        'S.N.T.G.N. TRANSGAZ S.A. - TGN',
+        'BANCA TRANSILVANIA S.A. - TLV',
+        'TRANSILVANIA INVESTMENTS ALLIANCE S.A. - TRANSI',
+        'TERAPLAST SA - TRP',
+        'TTS (TRANSPORT TRADE SERVICES) - TTS',
+        'TURISM FELIX S.A. - TUFE',
+        'VRANCART SA - VNC',
+        'PURCARI WINERIES PUBLIC COMPANY LIMITED - WINE',
+        'SOCEP S.A. - SOCP',
+        'LION CAPITAL S.A. - LION',
+        'PREFAB SA - PREH',
+        'TRANSILVANIA BROKER DE ASIGURARE - TBK',
+        'S.N.T.G.N. TRANSGAZ S.A. - TGN',
+        'C.N.T.E.E. TRANSELECTRICA - TEL',
+        'TURBOMECANICA S.A. - TBM',
+        'SANTIERUL NAVAL ORSOVA S.A. - SNO',
+        'SAFETECH INNOVATIONS - SAFE',
+        'FARMACEUTICA REMEDIA SA - RMAH',
+        'ROMPETROL WELL SERVICES S.A. - PTR',
+        'PREFAB SA - PREH',
+        'ONE UNITED PROPERTIES - ONE',
+        'INFINITY CAPITAL INVESTMENTS S.A. - INFINITY',
+        'TURISM, HOTELURI, RESTAURANTE MAREA NEAGRA S.A. - EFO',
+        'CHIMCOMPLEX S.A. BORZESTI - CRC',
+        'CONPET SA - COTE',
+        'LION CAPITAL S.A. - LION'
+        
     ];
 
     const handleChange = (event) => {
-        setSearchTerm(event.target.value.toLowerCase());
+        setSearchTerm(event.target.value.toUpperCase().trim());
     };
 
     const filteredCompanies = companies.filter(company =>
-        company.toLowerCase().includes(searchTerm)
+        company.toUpperCase().startsWith(searchTerm)
     );
 
     return (
@@ -46,13 +101,13 @@ const SearchableList = () => {
                 {searchTerm ? (
                     filteredCompanies.length > 0 ? (
                         filteredCompanies.map((company, index) => (
-                            <li key={index}>{company}</li>
+                            <li key={index} className='list-term'>{company}</li>
                         ))
                     ) : (
-                        <li>Nicio companie găsită</li>
+                        <li>Nicio companie nu a fost găsită</li>
                     )
                 ) : (
-                    <li>Introduceți un termen de căutare</li>
+                    <li></li>
                 )}
             </ul>
         </form>
